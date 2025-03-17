830a1daf7562aac2b5de872a3075e1a7a2c33508
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-03-17 12:12:26 +0100

    Change line positions to comply with instructions

--- assets/js/opgaver/domelementer.js
+++ assets/js/opgaver/domelementer.js
@@ -13,10 +13,10 @@ const yellow = document.getElementById('yellowFigure');
 Du skal ændre baggrundsfarve på de to elementer du har fundet i opgave 1.1
 farven på elementerne skal være blå. brug evt. elelement.style.backgroundColor
 */
-red.style.backgroundColor = 'blue';
-yellow.style.backgroundColor = 'blue';
 
 // din kode her
+red.style.backgroundColor = 'blue';
+yellow.style.backgroundColor = 'blue';
 
 /* opgave 2.1
 Du skal oprette følgende HTML elementer i Elementet med id opgaveTwo.
@@ -56,10 +56,11 @@ classArray.map((i) => {
 Brug din array til at ændre h3 elementets, inden i purpleFigures, tekst til RED. du kan bruge
 myElement.children[0].innerHTML til at ændre 1. child elements inner html.
 */
+
+// din kode her
 for (item in classArray) {
   classArray[item].innerHTML = 'RED';
 }
-// din kode her
 
 /* opgave 4
 opret DOM elementer i elementet med id opgaveFour, udfra data objectet myData.
